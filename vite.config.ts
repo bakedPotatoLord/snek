e218ad988bcf1e90d382dfdbc21bd3ab67a019e6
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-07-19 13:46:18 -0600

    configure build options

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {resolve,dirname} from 'node:path'
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const outDir = resolve(__dirname,'dist')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base:'/snek/',
+  build:{
+    outDir,
+  },
 })
