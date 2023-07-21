7b709286bc79641777c1bece258d90eb1d313e7f
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-07-20 19:17:54 -0600

    create global state managment class

---
+++ src/scripts/Game.ts
@@ -0,0 +1,17 @@
+import bImg from '../assets/images/background.png'
+import { createImage } from "./functions.js"
+
+export class Game{
+  static startTime= Date.now()
+	static paused= false
+	static rect= []
+	static over= false
+	static startPage= true
+	static bImg= createImage(bImg)
+  static active = true;
+
+
+  static on() {
+    return Game.active && !(Game.paused || Game.startPage || Game.over)
+  };
+}
