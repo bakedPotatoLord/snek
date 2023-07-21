cd0a376c7e091800fc2b1667939d0392279c7a0a
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-07-20 20:43:29 -0600

    create start function

--- src/scripts/Game.ts
+++ src/scripts/Game.ts
@@ -1,17 +1,20 @@
 import bImg from '../assets/images/background.png'
 import { createImage } from "./functions.js"
 
-export default class Game{
-  static startTime= Date.now()
-	static paused= false
-	static rect= []
-	static over= false
-	static startPage= true
-	static bImg= createImage(bImg)
+export default class Game {
+  static startTime = Date.now()
+  static paused = false
+  static rect = []
+  static over = false
+  static startPage = true
+  static bImg = createImage(bImg)
   static active = true;
-
 
   static on() {
     return Game.active && !(Game.paused || Game.startPage || Game.over)
   };
+  static start() {
+    Game.startPage = false;
+    Game.startTime = Date.now();
+  }
 }
