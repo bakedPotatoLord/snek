9a7831c6dd33a815e6fc29b6c90ed49d3cd1bc51
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-07-20 20:48:23 -0600

    reformatting

--- src/scripts/script.ts
+++ src/scripts/script.ts
@@ -1,8 +1,6 @@
 import Cookies from 'js-cookie'
 import { DefineComponent } from 'vue';
-import { createAudio, createImage } from './functions.js'
-//images
-import dirtImg from '../assets/images/dirt.png'
+import { createAudio } from './functions.js'
 
 //audio
 import eat from '../assets/audio/eat.wav';
@@ -10,42 +8,31 @@ import pauseAudio from '../assets/audio/pause.mp3';
 import theme1 from '../assets/audio/theme1.mp3';
 
 //game objects
-
 import Eagle from './Eagle.js';
 import Snek from './Snek.js';
 import Mouse from './Mouse.js';
 import Game from './Game.js';
 import Rocks from './Rocks.js';
+import Dirt from './sprites/Dirt.js'
 import { background, drawHearts, drawPause, drawReset, drawScore, gameOver, pause, pausedWords, play, resetImg, startPage } from './background.js';
 
 let eagle: Eagle
 let snek: Snek
 let mouse: Mouse
 let rocks: Rocks
+let dirt: Dirt
 
 let c: HTMLCanvasElement
 let ctx: CanvasRenderingContext2D
 
-function err(): never {
-	throw new Error("error");
-}
+const err =()=>{throw new Error("error")}
 
-if (Cookies.get('highscore') == undefined) {
-	Cookies.set('highscore', "0");
-};
-
-const dirt = {
-	'image': createImage(dirtImg),
-	'x': 0,
-	'y': 0,
-	'anim': 0,
-	'animating': true,
-}
+if (Cookies.get('highscore') == undefined) Cookies.set('highscore', "0")
 
 export const audio = {
-	'eat': createAudio(eat),
-	'pauseAudio': createAudio(pauseAudio),
-	'theme1': createAudio(theme1),
+	eat: createAudio(eat),
+	pauseAudio: createAudio(pauseAudio),
+	theme1: createAudio(theme1),
 }
 
 export const cw = 600
@@ -61,7 +48,6 @@ function reset() {
 	Game.startPage = false;
 	Game.over = false
 };
-
 function lifeReset() {
 	//reset mouse and eagle
 	mouse.reset()
@@ -70,11 +56,6 @@ function lifeReset() {
 	rocks.reset();
 	snek.life -= 1;
 }
-
-
-
-
-
 function logKey(event: KeyboardEvent) {
 	event.preventDefault()
 	console.log(event.code)
@@ -89,7 +70,6 @@ function logKey(event: KeyboardEvent) {
 	};
 	snek.drawHead()
 }
-
 function checkColissionLoop() {
 	if (snek.isAlive() &&
 		snek.checkBodyCollision() &&
@@ -108,42 +88,23 @@ function checkColissionLoop() {
 
 function displayLoop() {
 	if (Game.on()) {
-
 		snek.checkFoodCollision(mouse, dirt);
-
 		background(ctx);
 		rocks.draw()
-
-		drawScore(ctx,snek);
-		drawHearts(ctx,snek);
+		drawScore(ctx, snek);
+		drawHearts(ctx, snek);
 		drawPause(ctx);
 		drawReset(ctx);
-
 		snek.drawBody();
 		snek.drawHead();
 		mouse.draw(dirt);
-		drawDirt();
+		dirt.draw();
 		eagle.draw();
 	};
 	if (Game.paused) pausedWords(ctx);
 	if (Game.over) gameOver(ctx)
 	if (Game.startPage) startPage(ctx)
 };
-
-//****************************************//
-
-
-
-export function drawDirt() {
-	if (dirt.animating) {
-		let i = dirt.anim;
-		while (i >= 5) {
-			i -= 5;
-		}
-		ctx.drawImage(dirt.image, 0, i * 20, 20, 20, dirt.x, dirt.y, 20, 20);
-		dirt.anim -= 1;
-	}
-}
 
 export default <DefineComponent>{
 	mounted() {
@@ -155,32 +116,26 @@ export default <DefineComponent>{
 		snek = new Snek(ctx);
 		mouse = new Mouse(ctx);
 		rocks = new Rocks(ctx);
+		dirt = new Dirt(ctx);
 
 		c.width = cw;
 		c.height = ch + 60;
 
-		c.addEventListener('click', (event) => {
-
-			if (event.offsetX > pause.x && event.offsetX < pause.x + 32 && event.offsetY > pause.y && event.offsetY < pause.y + 32) {
-				if (Game.paused) {
-					Game.paused = false;
-				} else {
-					Game.paused = true;
-				};
-			};
-			if (Game.startPage == true) {
-				if (event.offsetX > play.x && event.offsetX < play.x + 128 && event.offsetY > play.y && event.offsetY < play.y + 128) {
-					console.log('play button clicked')
-					Game.startPage = false;
-					Game.startTime = Date.now();
-				};
-			};
-			if (event.offsetX > resetImg.x && event.offsetX < resetImg.x + 32 && event.offsetY > resetImg.y && event.offsetY < resetImg.y + 32) {
-				reset()
-			};
-			if (Game.over) {
-				reset();
-			};
+		c.addEventListener('click', event => {
+			if (event.offsetX > pause.x &&
+				event.offsetX < pause.x + 32 &&
+				event.offsetY > pause.y &&
+				event.offsetY < pause.y + 32) Game.paused = !Game.paused
+			if (Game.startPage &&
+				event.offsetX > play.x &&
+				event.offsetX < play.x + 128 &&
+				event.offsetY > play.y &&
+				event.offsetY < play.y + 128) Game.start()
+			if (event.offsetX > resetImg.x &&
+				event.offsetX < resetImg.x + 32 &&
+				event.offsetY > resetImg.y &&
+				event.offsetY < resetImg.y + 32) reset()
+			if (Game.over) reset();
 		});
 
 		eagle.reset();
