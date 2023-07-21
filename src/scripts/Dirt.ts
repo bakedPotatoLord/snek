2fed51c11ab926ee12c3fa149124917547531e1f
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-07-20 20:43:54 -0600

    create dirt class

---
+++ src/scripts/Dirt.ts
@@ -0,0 +1,21 @@
+import { createImage } from "./functions.js"
+import dirtImg from '../assets/images/dirt.png'
+
+
+export default class Dirt {
+  image = createImage(dirtImg)
+  x = 0
+  y = 0
+  anim = 0
+  animating = true
+  ctx: CanvasRenderingContext2D
+  constructor(ctx: CanvasRenderingContext2D) {
+    this.ctx = ctx
+  }
+  draw() {
+    if (this.animating) {
+      this.anim = (this.anim + 1) % 5;
+      this.ctx.drawImage(this.image, 0, this.anim * 20, 20, 20, this.x, this.y, 20, 20);
+    }
+  }
+}
